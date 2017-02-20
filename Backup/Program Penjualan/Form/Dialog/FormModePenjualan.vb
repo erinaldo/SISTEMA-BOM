1925dd30657875c36a9117571b44e3eb54261e22
R. H. Bayu Prabowo <rh.bayu.prabowo@outlook.com>
2017-02-21 01:47:40 +0700

    - Fix assembly Mysql.Data to version 6.9.8.0

--- Backup/Program Penjualan/Form/Dialog/FormModePenjualan.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FormModePenjualan
-
-    Private Sub RadButton1_Click(sender As Object, e As EventArgs) Handles RadButton1.Click, RadButton2.Click, RadButton3.Click
-        Me.Close()
-    End Sub
-End Class
--- Program Penjualan_Backup_2017.02.15_10.22.18/Form/Cetak/FormCetakNotaChecking.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class FormCetakNotaChecking
-    Property Invoice As String
-
-    Private Sub FormCetakNotaChecking_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.ReportNotaChecking1.ReportParameters("div").Value = DivKoefisien
-        Me.ReportNotaChecking1.ReportParameters("invoice").Value = Invoice
-        Me.ReportViewer1.RefreshReport()
-    End Sub
-End Class
